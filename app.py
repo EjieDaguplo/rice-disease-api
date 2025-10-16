4160fb3dc3956104c1b742e10a45fff8ee050a77
Ejie Daguplo <ejiedaguplo22@gmail.com>
2025-10-16 21:12:32 +0800

    Change the code of app.py and add gunicorn in the requirements.txt

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 # --- Load your trained model ---
 # Make sure 'best.pt' is in the same folder as this file
 model = YOLO("best.pt")
+model.fuse()    # Fuse model layers for better performance
 
 @app.route("/", methods=["GET"])
 def home():
@@ -27,33 +28,34 @@ def predict():
         filename = file.filename
         saved_path = os.path.join(temp_dir, filename)
         file.save(saved_path)
-
         print("Received file:", filename)
         print("Saved at:", saved_path)
 
         # Run YOLO prediction
-        results = model(saved_path)
+        results = model(saved_path, device='cpu')  # Use 'cuda' if GPU is available
+
+        # Clean up the saved file
+        os.remove(saved_path)
 
         detections = results[0].boxes
         if len(detections) == 0:
-            os.remove(saved_path)
             return jsonify({"result": "No disease detected"})
 
         top_detection = detections[0]
         label = results[0].names[int(top_detection.cls[0])]
         confidence = float(top_detection.conf[0])
 
-        os.remove(saved_path)
+        #os.remove(saved_path)
         return jsonify({
             "disease_name": label,
             "confidence": round(confidence, 3)
         })
 
     except Exception as e:
+        print("Error during prediction:", str(e))
         return jsonify({"error": str(e)}), 500
 
 
-if __name__ == "__main__":
-    # Run the app (localhost:5000)
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+    #port = int(os.environ.get("PORT", 10000))
+    #app.run(host="0.0.0.0", port=port)
